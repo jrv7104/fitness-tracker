21e8041243b752cb6070c36bff34f4f4f4eb7f69
Jason Viox <jrv04@hotmail.com>
2021-09-08 21:12:26 -0500

    Build Index.js framework

--- models/index.js
+++ models/index.js
@@ -1,5 +1,22 @@
+const Workout = require("./Workout");
+
 module.exports = {
     Workout: require("Workout.js"),
     Stats: require("Stats.js"),
     Exercise: require("Exercise.js")
 };
+
+//research .hasMany/belongsTo, add foreignKey
+Workout
+
+Workout
+
+Stats
+
+Stats
+
+Exercise
+
+Exercise
+
+module.exports = { Workout, Stats, Exercise };
