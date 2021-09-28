4b1dfe2dbff525a25e6d77460b00ee17a2efa55e
Jason Viox <jrv04@hotmail.com>
2021-09-27 20:18:42 -0500

    Build Workout.js api route

--- routes/api/Workout.js
+++ routes/api/Workout.js
@@ -1,13 +1,47 @@
 const router = require("express").Router();
 const { Exercise, Workout } = require("../../models");
 
-router.get("/:workouts", async (req, res) => {
-    try {
-        let workoutData = await Workout.findAll();
-        workoutData = workoutData.map((workout) => workout.get({ plain: true});
-        res.render("workout", { workoutData});
-    } catch (err) {
-        res.status(500).json(err);
-    }
+router.get("/", async (req, res) => {
+    Workout.findAll({
+        include: Exercise,
+        through: Exercise
+    }).then(workout => res.json(workout))
 });
 
+router.get('/:id', (req, res) => {
+    // find one category by its `id` value
+    // // be sure to include its associated Products
+    
+    Workout.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: Exercise,
+      through: Exercise
+    }).then(tag => res.json(tag))
+  });
+
+    router.post('/', (req, res) => {
+
+        Workout.create(req.body).then(newWorkout => res.json(newWorkout))
+  });
+
+  router.put('/:id', (req, res) => {
+      Workout.update(req.body, {
+          where: {
+              id: req.params.id
+          },
+          include: Exercise,
+          through: Exercise
+      }).then(updateWorkout => res.json(updateWorkout))
+  });
+
+  router.delete('/:id', (req, res) => {
+      Workout.destroy({
+          where: {
+              id: req.params.id
+          },
+      }).then(workout => res.json(workout))
+  });
+
+  module.exports = router;
