9e3ed4f1dd111bce8bae09cea7998f2e1d1df371
Jason Viox <jrv04@hotmail.com>
2021-09-27 20:38:30 -0500

    Build Exercise.js api route

--- routes/api/Exercise.js
+++ routes/api/Exercise.js
@@ -1,6 +1,43 @@
 const router = require("express").Router();
 const { Exercise, Workout } = require("../../models");
 
+router.get('/', (req, res) => {
+    Exercise.findAll({
+        include: Workout,
+        through: Workout
+    }).then(exercise => res.json(exercise))
+});
 
+router.get('/:id', (req, res) => {
+    Exercise.findOne({
+        where: {
+            id: req.params.id
+        },
+        include: Workout,
+        through: Workout
+    }).then(exercise => res.json(exercise))
+});
+
+router.post('/', (req, res) => {
+    Exercise.create(req.body).then(newExercise => res.json(newExercise))
+});
+
+router.put('/:id', (req, res) => {
+    Exercise.update(req.body, {
+        where: {
+            id: req.params.id
+        },
+        include: Workout,
+        through: Workout
+    }).then(updateExercise => res.json(updateExercise))
+});
+
+router.delete('/:id', (req, res) => {
+    Exercise.destroy({
+        where: {
+            id: req.params.id
+        },
+    }).then(exercise => res.json(exercise))
+});
 
 module.exports = router;
