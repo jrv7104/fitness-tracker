d16f3f0695f5f6eaeb657419037408a89f9f440a
Jason Viox <jrv04@hotmail.com>
2021-10-06 10:04:05 -0500

    Build aggregate, remove delete

--- routes/api/Workout.js
+++ routes/api/Workout.js
@@ -1,25 +1,20 @@
 const router = require("express").Router();
-const { Exercise, Workout } = require("../../models");
+const Workout = require("../../models/Workout");
 
-router.get("/", async (req, res) => {
-    Workout.findAll({
-        include: Exercise,
-        through: Exercise
-    }).then(workout => res.json(workout))
+router.get("/", (req, res) => {
+   Workout.aggregate(
+       [
+           {
+               $addFields: {
+                   totalDuration: {
+                       $sum: "$exercises.duration"
+                   }
+               }
+           }
+       ]
+   ).then(data => res.json(data))
+   .catch(err => res.status(500).json(err))
 });
-
-router.get('/:id', (req, res) => {
-    // find one category by its `id` value
-    // // be sure to include its associated Products
-    
-    Workout.findOne({
-      where: {
-        id: req.params.id
-      },
-      include: Exercise,
-      through: Exercise
-    }).then(tag => res.json(tag))
-  });
 
     router.post('/', (req, res) => {
 
@@ -27,21 +22,15 @@ router.get('/:id', (req, res) => {
   });
 
   router.put('/:id', (req, res) => {
-      Workout.update(req.body, {
-          where: {
-              id: req.params.id
-          },
-          include: Exercise,
-          through: Exercise
-      }).then(updateWorkout => res.json(updateWorkout))
+      Workout.findByIdAndUpdate( 
+        req.params.id,
+          {$push:{exercises: req.body}},
+          {new: true}
+      ).then(updateWorkout => res.json(updateWorkout))
+      .catch(err => res.status(500).json(err))
   });
 
-  router.delete('/:id', (req, res) => {
-      Workout.destroy({
-          where: {
-              id: req.params.id
-          },
-      }).then(workout => res.json(workout))
-  });
+// router.get('/range', (req,res) =>
+
 
   module.exports = router;
